cff355564e65ec2f08322221dc85f751de3dbf53
Vineeth Avvaru <avvaru95@gmail.com>
2020-06-24 00:45:34 +0900

    search bug fix

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -10,8 +10,11 @@ export const Reducer = (state = initialState, action) => {
     switch (action.type) {
         case ActionTypes.CHANGE_PAGE:
             return {...state, currentPage: action.payload}
-        case ActionTypes.HANDLE_SEARCH: 
-            return {...state, searchTitle: action.payload}
+        case ActionTypes.HANDLE_SEARCH:
+            let lastChar = action.payload[action.payload.length-1];
+            if(lastChar === " ") state = {...state, searchTitle: action.payload}
+            else  state = {...state, searchTitle: action.payload, currentPage: 1}
+            return state;
         default:
             return state;
     }
